7af306c54b4ba88a58bd86679501884bc23ac140
norm02 <richa0920@gmail.com>
2023-07-04 14:27:16 +0900

    Updates

--- client/App.js
+++ client/App.js
@@ -1,8 +1,7 @@
+import { jsx as _jsx } from "react/jsx-runtime";
 import { Home } from "./pages/Home";
 import "./App.css";
-
 function App() {
-  return <Home />;
+    return _jsx(Home, {});
 }
-
 export default App;
